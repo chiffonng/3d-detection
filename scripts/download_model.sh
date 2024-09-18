e15c3a13c56f02a1a79c418a1745a82501655b87
My (Chiffon) Nguyen <s-tngu18@haw-landshut.de>
2024-09-18 16:16:07 +0200

    fix sourcing of `utils.sh` and add a check to skip the download if the model already exists

--- scripts/download_model.sh
+++ scripts/download_model.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-source ./utils.sh
+# shellcheck source=./scripts/utils.sh
+source ./scripts/utils.sh
 
 # Check that wget and unzip are installed
 if ! [ -x "$(command -v wget)" ]; then
@@ -11,14 +12,22 @@ fi
 
 if [ "$1" == "train" ] || [ "$1" == "fine-tune" ] || [ "$1" == "t" ] || [ "$1" == "ft" ]; then
   MODEL_URL="https://api.ngc.nvidia.com/v2/models/nvidia/tao/pointpillarnet/versions/trainable_v1.1/zip"
-  MODEL_NAME="pointpillarnet_trainable.zip"
+  MODEL_ZIP="pointpillarnet_trainable.zip"
+  MODEL_NAME="pointpillarnet_trainable.tlt"
 else
   MODEL_URL="https://api.ngc.nvidia.com/v2/models/nvidia/tao/pointpillarnet/versions/deployable_v1.0/zip"
-  MODEL_NAME="pointpillarnet_deployable.zip"
+  MODEL_ZIP="pointpillarnet_deployable.zip"
+  MODEL_NAME="pointpillarnet_deployable.etlt"
+fi
+
+# Check if the model already exists inside "models" folder
+if [ -d "models/$MODEL_NAME" ]; then
+  warning "Model already exists in models folder. Skipping download."
+  exit 0
 fi
 
 # Download the model
-info "Download pre-trained model..."
-wget --content-disposition $MODEL_URL -O $MODEL_NAME
-unzip $MODEL_NAME -d models
-rm $MODEL_NAME
+info "Downloading pre-trained model..."
+wget --content-disposition $MODEL_URL -O $MODEL_ZIP
+unzip $MODEL_ZIP -d models
+rm $MODEL_ZIP
